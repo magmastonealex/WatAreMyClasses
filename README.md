0b6a52f85565a8547f62008a13e6863784e9a6e3
Alex Roth <me@magmastone.net>
2015-07-28 18:17:17 -0400

    Added requirements.txt, comments

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # WatAreMyClasses
 Campus Navigation App for UW
 
+Getting set up
+---
+- `pip install -r requirements.txt`
+
 
 Current todos:
 
-- Get the nodes into something that a pathfinding algorithm can use.
-   - Use the building center point as a new node.
+- ~~Get the nodes into something that a pathfinding algorithm can use.~~ Done (I think?)
+   - Find relationships between nodes.
+   - ~~Use the building center point as a new node.~~ Done!
    - From there, just use pathfinding from a given node to a building's node, or another path node.
+      - This is of course, more complicated than it sounds
--- process.py
+++ process.py
@@ -1,38 +1,61 @@
 import xml.etree.ElementTree as ET
 from numpy import *
-
 from qhull_2d import *
 from min_bounding_rect import *
 
+INF=10**10
+
+def floydwarshall(V, edges): #number of edges,
+	dist=[[INF]*V for i in range(V)]
+
+	for i in range(V):
+		dist[i][i] = 0
+	
+	for (start,end,distance) in edges:
+		dist[start][end] = distance
+
+	for k in range(V):
+		for j in range(V):
+			for i in range(V):
+				if dist[i][j] > dist[i][k] + dist[k][j]:
+					dist[i][j] = dist[i][k] + dist[k][j]
+
+	return dist
+
+
+
 tree = ET.parse('mapdata.xml')
 root = tree.getroot()
+
+#Dictionary of IDs to coords
 nodes={}
+
 # Get all nodes
 for node in root.findall(".//node"):
 	nodes[node.get("id")]=[float(node.get("lat")),float(node.get("lon"))]
 
-#Bounding box start
 
 points=[]
-for node in root.findall('.//way/tag[@v="Engineering 3"]/../nd'): # Find Eng3 and get points
+for node in root.findall('.//way/tag[@v="Engineering 3"]/../nd'): # Find Eng3 and get boundary points
 	points.append(nodes[node.get("ref")])
 
 #Calculate a bounding box
 xy_points = array(points)
-
 hull_points = qhull2D(xy_points)
-
 hull_points = hull_points[::-1]
 
 (rot_angle, area, width, height, center_point, corner_points) = minBoundingRect(hull_points)
 print "Bounding Box of Eng3:"
-#ngraph is all nodes. It will need some back-processing to figure out the path it came from. That's up next!
-ngraph=[]
-ngraph.append({"id":"b-eng3","coords":[center_point[0],center_point[1]]}) # Get it out of numpy.
 print corner_points
 
 
-#Get paths + points
+#ngraph is all nodes. It will need some processing to figure out the path it came from.
+ngraph=[]
+ngraph.append({"id":"b-eng3","coords":[center_point[0],center_point[1]]}) # Get it out of numpy.
+
+
+
+#Get paths and associated points
 paths=[]
 for node in root.findall('.//way/tag[@v="path"]/..')+root.findall('.//way/tag[@v="footway"]/..'):
 	path=[]
@@ -40,8 +63,5 @@ for node in root.findall('.//way/tag[@v="path"]/..')+root.findall('.//way/tag[@v
 		path.append({"id":point.get("ref"),"coords":nodes[point.get("ref")]})
 		ngraph.append({"id":point.get("ref"),"coords":nodes[point.get("ref")]})
 	paths.append({"id":node.get("id"),"nodes":path})
-
-
-
 
 print str(len(paths))+" paths found"
