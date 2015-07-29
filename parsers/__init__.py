f672191c7886b8ce49f3970efc56074709dfcbb6
Alex Roth <me@magmastone.net>
2015-07-28 22:54:13 -0400

    Document,Document,Document

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,1 +1,2 @@
 from osm import osm
+from quest import quest
---
+++ parsers/quest.py
@@ -0,0 +1,28 @@
+import re
+def uw_waterloo_quest_schedule(input):
+	arr_days={'m':"MO",'t':"TU","w":"WE","h":"TH","f":"FR","s":"SA","u":"SU"}
+	arr_num_days={'m':1,"t":2,"w":3,"h":4,"f":5,'s':6,"u":7}
+
+	code=""
+	name=""
+	section=""
+	typ=""
+	days=""
+	time_start=""
+	time_end=""
+	location=""
+	prof=""
+	date_start=""
+	date_end=""
+
+	ical_array=[]
+	js_array=[]
+	pos=0
+	total_length=len(input)
+	swap=True
+
+	while pos < total_length and pos >=0:
+		found=False
+		ampm=False
+
+		if re.match('/(AM)|(PM)/', ) != None
