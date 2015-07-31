977a2cb4d1bc437bfa3e5b5a337b28ffdc86e863
Alex Roth <me@magmastone.net>
2015-07-31 15:35:35 -0400

    In line with new schema and data format

--- process.py
+++ process.py
@@ -4,8 +4,13 @@ from parsers import osm
 from viewer import NodeCollection,Node
 
 nC=NodeCollection()
-nC.load()
-nC.dedupe()
-nC.save()
-pF=pathFinder(nC)
-pF.build()
+nC.load("out.nodecollection")
+nds=open("nodes.sql","w")
+ndsR=open("nodes.redis","w")
+for nodeID,node in nC.vertices.items():
+	nds.write("INSERT INTO nodes (id,name,lat,long) VALUES ('"+nodeID+"','"+node.name+"',"+str(node.x)+","+str(node.y)+");\n")
+	ndsR.write("SET 'nodes:"+nodeID+"' '"+str(node.x)+","+str(node.y)+","+node.name+"'\n")
+nds.close()
+ndsR.close()
+#pF=pathFinder(nC)
+#pF.build()
