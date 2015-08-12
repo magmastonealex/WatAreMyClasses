7cfd787771cb6fdb80ac2c6071c6433cebb0cf95
Alex Roth <science9711@gmail.com>
2015-08-12 13:06:25 -0400

    Documentation

--- iOS/WatAreMyClasses/WatAreMyClasses/ViewController.m
+++ iOS/WatAreMyClasses/WatAreMyClasses/ViewController.m
@@ -25,35 +25,36 @@ double curLong;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //AlzaSyANOLtR9yMGFcquqC0M6BvlUMTmgLUwoX4
-    //
-    GMSCameraPosition * camera = [GMSCameraPosition cameraWithLatitude:43.470366  longitude:-80.541945 zoom:18 bearing:0 viewingAngle:80];
     
-    _mapView.settings.myLocationButton = TRUE;
-    _mapView.camera=camera;
-    _mapView.myLocationEnabled=true;
-    if(locationmanager==nil){
-        locationmanager = [[CLLocationManager alloc] init];
+    GMSCameraPosition * camera = [GMSCameraPosition cameraWithLatitude:43.470366  longitude:-80.541945 zoom:18 bearing:0 viewingAngle:80]; // Setup our initial camera position over Waterloo.
+    
+    _mapView.settings.myLocationButton = TRUE; // Let users find themselves.
+    _mapView.camera=camera; // Move to our initial camera.
+    _mapView.myLocationEnabled=true; // Let users find themselves.
+    if(locationmanager==nil){ // Probably yes, but not always.
+        locationmanager = [[CLLocationManager alloc] init]; // Make a new LocationManager so that we can get location info.
     }
-    [locationmanager requestWhenInUseAuthorization];
-    locationmanager.delegate=self;
-    locationmanager.desiredAccuracy=kCLLocationAccuracyBest;
-    locationmanager.distanceFilter=1;
+
+    [locationmanager requestWhenInUseAuthorization]; // New in iOS 8
+    locationmanager.delegate=self; // We want to hear about location & orientation updates.
+    locationmanager.desiredAccuracy=kCLLocationAccuracyBest; // We're doing path-based navigation. Need high accuracy.
+    locationmanager.distanceFilter=1; // If user moves more than 1 metre, call our delegate method.
     
     [locationmanager startUpdatingLocation];
     [locationmanager startUpdatingHeading];
     
-    DataCacher * cache=[DataCacher sharedCache];
-    cache.userID=@"aj3roth";
-    cache.token=@"mfGVvr3AJT9wMGYESuz5gwRcGnEwumrKTf27UARQ3MVIrOhuEnZa8llBDHL89w13";
-    [cache doCache];
+    DataCacher * cache=[DataCacher sharedCache]; // Singleton to cache web-based data
+    cache.userID=@"aj3roth"; // Needs to be collected via QR code & stored in CacheManager, like in Android.
+    cache.token=@"mfGVvr3AJT9wMGYESuz5gwRcGnEwumrKTf27UARQ3MVIrOhuEnZa8llBDHL89w13"; // Hey, that's mine ! :)
+    [cache doCache]; // Prepare the cache for when a view-controller needs it later.
 
     // Do any additional setup after loading the view, typically from a nib.
 }
+//Draw a Polyline on the map, removing the previous one. Uses user's last-known location.
 -(void)doPathPolyline:(NSString *)to{
-    WatService * svc=[WatService sharedService];
+    WatService * svc=[WatService sharedService]; // Another singleton. Making these is very expensive on iOS.
     [svc getClosestNodeWithLat:curLat lon:curLong completion:^(OVCResponse *resp, NSError *error) {//TODO: error handing.
-        [svc getPathFromNode:((WatNode*)resp.result).ndid toNode:to completion:^(OVCResponse *resp, NSError *error) {
+        [svc getPathFromNode:((WatNode*)resp.result).ndid toNode:to completion:^(OVCResponse *resp, NSError *error) { //TODO: Error handling.
             GMSMutablePath*gmp = [GMSMutablePath path];
             for (WatNode*nde in resp.result) {
                 NSLog(@"Path along: %@",nde.ndid);
@@ -71,28 +72,28 @@ double curLong;
 }
 
 -(void) locationManager:(CLLocationManager*) manager didUpdateHeading:(nonnull CLHeading *)newHeading{
-    [_mapView animateToBearing:newHeading.trueHeading];
+    [_mapView animateToBearing:newHeading.trueHeading]; // trueHeading is true north like Gmaps expects.
 }
 -(void) locationManager:(CLLocationManager *)manager didUpdateLocations:(nonnull NSArray<CLLocation *> *)locations{
-    CLLocation * loc=[locations lastObject];
-    curLat=loc.coordinate.latitude;
+    CLLocation * loc=[locations lastObject]; //Get most recent location.
+    curLat=loc.coordinate.latitude; // Update for pathfinding.
     curLong=loc.coordinate.longitude;
-    NSLog(@"Location Update: %f,%f",loc.coordinate.latitude,loc.coordinate.longitude);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)unwindFromClasses:(UIStoryboardSegue *)unwindSegue
+
+- (IBAction)unwindFromClasses:(UIStoryboardSegue *)unwindSegue // Callback from ClassesViewController
 {
-    NSString *bid=((ClassesViewController*)unwindSegue.sourceViewController).doneGoto;
+    NSString *bid=((ClassesViewController*)unwindSegue.sourceViewController).doneGoto; //NodeID for desired building
     NSLog(@"Class: %@",bid);
     [self doPathPolyline:bid];
 }
-- (IBAction)unwindFromBuildings:(UIStoryboardSegue *)unwindSegue
+- (IBAction)unwindFromBuildings:(UIStoryboardSegue *)unwindSegue// Callback from BuildingsViewController
 {
-    NSString * bid=((BuildingsViewController*)unwindSegue.sourceViewController).doneGoto;
+    NSString * bid=((BuildingsViewController*)unwindSegue.sourceViewController).doneGoto; // NodeID for desired building
     NSLog(@"Buildings: %@",bid);
     [self doPathPolyline:bid];
     
